46fe6b294f30330e74048738e485a650648630b1
tung0413 <rpg55666@gmail.com>
2024-02-27 16:58:06 +0800

    [ Feat ] 完成航班資訊Item UI

--- app/src/main/java/com/example/airportfly/ui/flight/FlightFragment.kt
+++ app/src/main/java/com/example/airportfly/ui/flight/FlightFragment.kt
@@ -1,4 +1,4 @@
-package com.example.airportfly.ui.dashboard
+package com.example.airportfly.ui.flight
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.airportfly.databinding.FragmentDashboardBinding
+import com.example.airportfly.databinding.FragmentFlightBinding
 
-class DashboardFragment : Fragment() {
+class FlightFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentFlightBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val flightViewModel =
+            ViewModelProvider(this).get(FlightViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentFlightBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textFlight
+        flightViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
