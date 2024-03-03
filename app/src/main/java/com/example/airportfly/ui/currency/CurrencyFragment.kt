84b7974d0469fcdd6764a8134a77c2401e2cddd4
tung0413 <rpg55666@gmail.com>
2024-03-03 23:32:06 +0800

    [ Feat ] 完成匯率UI

--- app/src/main/java/com/example/airportfly/ui/currency/CurrencyFragment.kt
+++ app/src/main/java/com/example/airportfly/ui/currency/CurrencyFragment.kt
@@ -28,11 +28,6 @@ class CurrencyFragment : Fragment() {
 
         _binding = FragmentCurrencyBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textCurrency
-        currencyViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
